a7b4c25c44015202266b0e0afe62f6c967adff21
nawata <nawataster@gmail.com>
2015-03-15 20:43:40 +0200

    Accordion nesting
    Accordion nesting example.

--- resources/views/dashboard/categories.blade.php
+++ resources/views/dashboard/categories.blade.php
@@ -65,7 +65,15 @@
     </ul>
   </div>
 </div>
-<?php */ ?>
+
+
+
+
+
+
+
+
+
 
 <div class="accordion-group">
 <div class="accordion-heading">
@@ -92,6 +100,93 @@
 </div>
 </div>
 
+<?php */ ?>
+
+
+
+
+
+
+
+
+
+
+
+    <div class="panel-group" id="accordion">
+
+
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">1. What is HTML?</a>
+                </h4>
+            </div>
+
+            <div id="collapseOne" class="panel-collapse collapse in">
+                <div class="panel-body">
+                    <p>HTML stands for HyperText Markup Language. HTML is the main markup language for describing the structure of Web pages. <a href="http://www.tutorialrepublic.com/html-tutorial/" target="_blank">Learn more.</a></p>
+
+
+
+
+
+
+        <div class="panel-group" id="accordion1">
+	        <div class="panel panel-default">
+	            <div class="panel-heading">
+	                <h4 class="panel-title">
+	                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapseTwo_1">2. What is Bootstrap?</a>
+	                </h4>
+	            </div>
+	            <div id="collapseTwo_1" class="panel-collapse collapse">
+	                <div class="panel-body">
+	                    <p>Bootstrap is a powerful front-end framework for faster and easier web development. It is a collection of CSS and HTML conventions. <a href="http://www.tutorialrepublic.com/twitter-bootstrap-tutorial/" target="_blank">Learn more.</a></p>
+	                </div>
+	            </div>
+	        </div>
+		</div>
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">2. What is Bootstrap?</a>
+                </h4>
+            </div>
+            <div id="collapseTwo" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>Bootstrap is a powerful front-end framework for faster and easier web development. It is a collection of CSS and HTML conventions. <a href="http://www.tutorialrepublic.com/twitter-bootstrap-tutorial/" target="_blank">Learn more.</a></p>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">3. What is CSS?</a>
+                </h4>
+            </div>
+            <div id="collapseThree" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>CSS stands for Cascading Style Sheet. CSS allows you to specify various style properties for a given HTML element such as colors, backgrounds, fonts etc. <a href="http://www.tutorialrepublic.com/css-tutorial/" target="_blank">Learn more.</a></p>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
 
 
 @stop
