a49a3dbf5c88f18626be2865abaf13e0d2fc48b1
nawata <nawataster@gmail.com>
2015-03-22 08:54:49 +0200

    before composer
    before composer require illuminate/html

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,12 @@ use App\Category;
 
 class DashboardController extends MainController{
 
-    public function getCategories(){
+/**
+ * shows categories tree and seleceted category (if it was selected)
+ * @param integer $selCatId	- category id.
+ * @return \Illuminate\View\View - HTML content
+ */
+    public function getCategories( $selCatId=NUll ){
     	$tree	= Category::getTree();
 
 
@@ -25,6 +30,10 @@ class DashboardController extends MainController{
     public function getCategory( $id ){
     	$cat = Category::find( $id );
     	return view( 'dashboard/cat_content', ['cat'=>$cat] );
+    }
+//______________________________________________________________________________
+
+    public function postCategory(){
     }
 //______________________________________________________________________________
 
--- resources/views/dashboard/cat_content.blade.php
+++ resources/views/dashboard/cat_content.blade.php
@@ -1,5 +1,11 @@
 <div class="form-group">
 
+<div class="raw">
+
+{{ Form::open(array('url'=>action('DashboardController@postCategory'), 'method'=>'post', 'role'=>'form', 'class'=>'form-horizontal')) }}
+
+
+	<input type="hidden" value="{{ $cat->id }}">
 
 	<div class="col-sm-3">
     	<label for="sector" class="control-label">{{ @trans('prompts.name') }}</label>
@@ -8,5 +14,7 @@
 		<input type="text" class="form-control" value="{{ $cat->name }}">
     </div>
 
+{{ Form::close() }}
+</div>
 
 </div>
