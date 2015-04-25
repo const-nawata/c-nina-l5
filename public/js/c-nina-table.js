1301c7c79c4f996ae10202c764605a8b4ace853c
nawata <nawataster@gmail.com>
2015-04-25 15:38:24 +0300

    JQuery cNinaTable component
    Global variables re-factor.

--- public/js/c-nina-table.js
+++ public/js/c-nina-table.js
@@ -1,35 +1,4 @@
-/**
- *
- */
-
-//var tbl_prompts = {
-//	"emptyTable":prompts.empty_table
-//	,"infoEmpty":prompts.info_empty
-//	,"info":prompts.info
-//	,"infoFiltered":prompts.info_filtered
-//	,"zeroRecords":prompts.zero_records
-//	,"search":""
-//	,"lengthMenu":prompts.length_menu
-//	,"processing":prompts.processing
-//	,"loadingRecords": prompts.loading_records
-//
-//    ,"paginate":{
-//        "first":prompts.paginate.first,
-//        "last":prompts.paginate.last,
-//
-////        "next":prompts.paginate.next,
-////        "previous":prompts.paginate.previous
-//
-//        "next":"&raquo;",
-//        "previous":"&laquo;"
-//    }
-//};
-
 (function($){
-	$.fn.pid	= null;
-	$.fn.tbl	= null;
-	$.fn.isIndSearch	= false;
-
     $.fn.extend({
 /**
  * extends DataTable functionality.
@@ -38,11 +7,16 @@
  * @returns	void
  */
     	cNinaTable: function(pT, pE){
-    		$.fn.pid	= $(this).attr("id");
-    		$.fn.isIndSearch	= typeof pE.searchCols != "undefined" && pE.searchCols.length > 0;
+
+    		var table
+    			,pid		= $(this).attr("id")
+    			,isIndivSch	= false
+    			;
+
+    		isIndivSch	= typeof pE.searchCols != "undefined" && pE.searchCols.length > 0;
 
     		pT.ajax.data=function(srvData){//Data to send to server
- 	            srvData.pid	= $.fn.pid;
+ 	            srvData.pid	= pid;
          	};
 
          	pT.language	= {
@@ -69,44 +43,44 @@
          	};
 
 
-         	$.fn.tbl	= $(this).DataTable(pT);//						Start table creating ###############################################
+         	table	= $(this).DataTable(pT);//						Start table creating ###############################################
 
-		    $("#"+$.fn.pid+"_filter input").unbind().on("keyup change", function(e){//Change main search input handler
+		    $("#"+pid+"_filter input").unbind().on("keyup change", function(e){//Change main search input handler
 				(e.keyCode == 13) ? execTblSearch():null;
 			}).addClass("form-control").attr("placeholder", prompts.search );
 
-		    $("#"+$.fn.pid+"_length select").addClass("form-control");
+		    $("#"+pid+"_length select").addClass("form-control");
 
-		    $("#"+$.fn.pid+"_filter")
-		    	.prepend("<button id='"+$.fn.pid+"_search_btn'></button>")//Main search button
-		    	.append("<button id='"+$.fn.pid+"_clean_btn'></button>")//Clean button for main search
-		    	.prepend("<span id='"+$.fn.pid+"_tools' class='tbl-tool-btns-span'></span>");//	Tool buttons container
+		    $("#"+pid+"_filter")
+		    	.prepend("<button id='"+pid+"_search_btn'></button>")//Main search button
+		    	.append("<button id='"+pid+"_clean_btn'></button>")//Clean button for main search
+		    	.prepend("<span id='"+pid+"_tools' class='tbl-tool-btns-span'></span>");//	Tool buttons container
 
-		    $("#"+$.fn.pid+"_search_btn").button({				//Settings of Main search button
+		    $("#"+pid+"_search_btn").button({				//Settings of Main search button
 				icons: { primary: "ui-icon-search" },
 				text: false
 			}).on("click", function(e) {
 				execTblSearch();
 			}).attr("title", prompts.exec_search);
 
-		    $("#"+$.fn.pid+"_clean_btn").button({				//Settings of Clean button for main search
+		    $("#"+pid+"_clean_btn").button({				//Settings of Clean button for main search
 				icons: { primary: "ui-icon-cancel" },
 				text: false
 			}).on("click", function(e) {
-				$("#"+$.fn.pid+"_filter input").val("");
+				$("#"+pid+"_filter input").val("");
 				execTblSearch();
 			}).attr("title", prompts.clean);
 
-			if( $.fn.isIndSearch ){								//Set handlers for individual search inputs
+			if( isIndivSch ){								//Set handlers for individual search inputs
 
 				for(var cn in pE.searchCols ){
-					$("input", $.fn.tbl.column(pE.searchCols[cn]).footer()).attr("id", $.fn.pid+"_inp_"+pE.searchCols[cn]);
+					$("input", table.column(pE.searchCols[cn]).footer()).attr("id", pid+"_inp_"+pE.searchCols[cn]);
 
-					$(".ind-clean-btn", $.fn.tbl.column(pE.searchCols[cn]).footer())
-						.attr("id", $.fn.pid+"cleanbtn-"+pE.searchCols[cn])
+					$(".ind-clean-btn", table.column(pE.searchCols[cn]).footer())
+						.attr("id", pid+"cleanbtn-"+pE.searchCols[cn])
 						.on( "click", function(e){
 							var idd	= $(this).attr("id").split("-");
-							$("#"+$.fn.pid+"_inp_"+idd[1]).val("");
+							$("#"+pid+"_inp_"+idd[1]).val("");
 				        	execTblSearch()
 						});
 				}
@@ -134,17 +108,17 @@
 			}
 
 
-			obj	= $("#"+$.fn.pid+" .all-check");				//All-checking check-box.   rowcheckbox-
+			obj	= $("#"+pid+" .all-check");				//All-checking check-box.   rowcheckbox-
 
 			if(obj){
 				obj.on("click", function(e){		//Set onclick handler on "all rows" check-box
-					$("#"+$.fn.pid+" .row-check-box").prop('checked', $(this).is(':checked'));
+					$("#"+pid+" .row-check-box").prop('checked', $(this).is(':checked'));
 					setDelBtnState();
 				}).prop('checked', false);
 
 				//	There is no need in archive button if there are no row selboxes.						Delete
-				$("#"+$.fn.pid+"_tools").prepend("<button id='"+$.fn.pid+"_arch_btn'></button>");
-				$("#"+$.fn.pid+"_arch_btn").button({
+				$("#"+pid+"_tools").prepend("<button id='"+pid+"_arch_btn'></button>");
+				$("#"+pid+"_arch_btn").button({
 					icons: { primary: "ui-icon-locked" },
 					text: false
 				}).on( "click", function(e){
@@ -152,7 +126,7 @@
 					,ids=[]
 					;
 
-					$("#"+$.fn.pid+" .row-check-box").each(function(){
+					$("#"+pid+" .row-check-box").each(function(){
 						var idd;
 
 						if( $(this).is(':checked') ){
@@ -176,7 +150,7 @@
 
 				        	inform( prompts.op_result, resp.message );
 
-				        	$.fn.tbl.ajax.reload(function(json){
+				        	table.ajax.reload(function(json){
 				        		setDelBtnState();
 				        	});
 				        },
@@ -190,17 +164,17 @@
 
 				$( document ).ajaxComplete(function(){// Row check-boxes handlers.
 
-					$("#"+$.fn.pid+" .row-check-box").on("click", function(e){
+					$("#"+pid+" .row-check-box").on("click", function(e){
 						var all_checked=true;
 
-						$("#"+$.fn.pid+" .row-check-box").each(function(){
+						$("#"+pid+" .row-check-box").each(function(){
 							if( !$(this).is(':checked') ){
 								all_checked	= false;
 								return false;
 							}
 						});
 
-						$("#"+$.fn.pid+" .all-check").prop('checked', all_checked );
+						$("#"+pid+" .all-check").prop('checked', all_checked );
 
 						setDelBtnState();
 					});
@@ -211,8 +185,8 @@
 			}
 
 			//	Add new record button
-			$("#"+$.fn.pid+"_tools").prepend("<button id='"+$.fn.pid+"_add_btn'></button>");
-			$("#"+$.fn.pid+"_add_btn").button({
+			$("#"+pid+"_tools").prepend("<button id='"+pid+"_add_btn'></button>");
+			$("#"+pid+"_add_btn").button({
 				icons: { primary: "ui-icon-circle-plus" },
 				text: false
 			}).on( "click", function(e){
@@ -221,14 +195,14 @@
 
 
 			//	Handler for edit row.
-			$("#"+$.fn.pid+" tbody").on( 'click', 'td', function(){
-				$("#"+$.fn.pid+" .selected").removeClass('selected');
+			$("#"+pid+" tbody").on( 'click', 'td', function(){
+				$("#"+pid+" .selected").removeClass('selected');
 
 				if ( !$(this).hasClass('unclickable') ){
-					$("#"+$.fn.pid+" .row-check-box").prop('checked', false);
+					$("#"+pid+" .row-check-box").prop('checked', false);
 					$(this).parent('tr').addClass('selected');
 
-					showTblRecForm( $.fn.tbl.cell( '.selected', 0 ).data() );
+					showTblRecForm( table.cell( '.selected', 0 ).data() );
 				}
 			});
 
@@ -236,7 +210,6 @@
 
 
 //	#######################	FUNCTIONS	###########################################################
-
 			/**
 			 * calls TableDates searching utility
 			 * @param DataTable table - object in which shearch is performed
@@ -244,13 +217,13 @@
 			 * @return void
 			 */
 			function execTblSearch(){
-				$.fn.tbl.search( $("#"+$.fn.pid+"_filter input").val() );
+				table.search( $("#"+pid+"_filter input").val() );
 
-				if( $.fn.isIndSearch )
+				if( isIndivSch )
 					for(var cn in pE.searchCols )
-						$.fn.tbl.column(pE.searchCols[cn]).search( $('#'+$.fn.pid+"_inp_"+pE.searchCols[cn]).val());
+						table.column(pE.searchCols[cn]).search( $('#'+pid+"_inp_"+pE.searchCols[cn]).val());
 
-				$.fn.tbl.draw();
+				table.draw();
 			}
 			//------------------------------------------------------------------
 
@@ -263,7 +236,7 @@
 				var none_checked = true
 					,fade_level=0.5;
 
-				$("#"+$.fn.pid+" .row-check-box").each(function(){
+				$("#"+pid+" .row-check-box").each(function(){
 					if( $(this).is(':checked') ){
 						none_checked	= false;
 						fade_level = 1.0;
@@ -271,7 +244,7 @@
 					}
 				});
 
-				$("#"+$.fn.pid+"_arch_btn").attr( "disabled", none_checked ).fadeTo( "fast", fade_level );
+				$("#"+pid+"_arch_btn").attr( "disabled", none_checked ).fadeTo( "fast", fade_level );
 
 			}
 			//------------------------------------------------------------------
@@ -286,7 +259,7 @@
 
 				var tst
 				,id_url	= id == null ? "" : "/"+id
-				,dform_id = $.fn.pid+"-form-dialog"
+				,dform_id = pid+"-form-dialog"
 				,dform
 				;
 
@@ -298,7 +271,7 @@
 					dform	= $("#"+dform_id);
 
 				$.ajax({
-					url: pE.urls.form+"/"+$.fn.pid+id_url,
+					url: pE.urls.form+"/"+pid+id_url,
 					success: function(result){
 						dform.html( result );
 					},
@@ -320,7 +293,7 @@
 					   {
 						text: prompts.save,
 						click: function(){
-							$("#"+$.fn.pid+"form").submit();
+							$("#"+pid+"form").submit();
 						}
 					}
 					]
@@ -328,7 +301,7 @@
 
 				$( document ).ajaxComplete(function(){
 
-					$("#"+$.fn.pid+"form").submit(function(e){
+					$("#"+pid+"form").submit(function(e){
 
 						if( !is_submit ){
 							is_submit	= true;
@@ -340,7 +313,7 @@
 						        success:function(data, textStatus, jqXHR){
 						        	dform.dialog("close");
 						        	inform( prompts.op_result, messages.save_success );//TODO: Get message from server. Like in remove handler.
-						        	$.fn.tbl.ajax.reload(null,false);
+						        	table.ajax.reload(null,false);
 						        },
 
 						        error: function(jqXHR, textStatus, errorThrown){
