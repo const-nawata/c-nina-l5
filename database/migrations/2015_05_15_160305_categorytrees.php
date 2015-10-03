e840078aaf40d538ada37ad77240f59552324589
Constantine <nawataster@gmail.com>
2015-10-03 14:02:13 +0300

    Migration corrections
    Corrections in category tree table.

--- database/migrations/2015_05_15_160305_categorytrees.php
+++ database/migrations/2015_05_15_160305_categorytrees.php
@@ -11,18 +11,24 @@ class Categorytrees extends Migration {
 	 * @return void
 	 */
 	public function up(){
-		Schema::dropIfExists('categorytrees');
+		Schema::dropIfExists('categorytree');
 
-		Schema::create( 'categorytrees', function( Blueprint $table ){
+		Schema::create( 'categorytree', function( Blueprint $table ){
 			$table->engine = 'InnoDB';
 			$table->increments( 'id' )->unsigned();
 			$table->integer( 'category_id',FALSE, TRUE);
-			$table->integer( 'parent_id',FALSE, TRUE);
+			$table->integer( 'parent_id',FALSE, TRUE)->nullable()->default(NULL);
+			$table->integer( 'rank', FALSE, TRUE )->default(0);
 
 			$table->foreign('category_id','categrory_tree_fk')
 				->references('id')->on('categories')
 				->onDelete('cascade')
 				->onUpdate('cascade');
+
+			$table->foreign('parent_id','parent_tree_fk')
+				->references('id')->on('categorytree')
+				->onDelete('set null')
+				->onUpdate('set null');
 
 		});
 	}
@@ -33,7 +39,7 @@ class Categorytrees extends Migration {
 	 * @return void
 	 */
 	public function down(){
-		Schema::dropIfExists('categorytrees');
+		Schema::dropIfExists('categorytree');
 	}
 
 }
